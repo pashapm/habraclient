0a5e7b055067c3cdee21891efca917d0126c2d9a
wnezros@gmail.com <wnezros@gmail.com@08069db9-3981-113e-16f8-449106ddf22e>
2011-04-26 11:08:52 +0000

    <object> to <a> by preferences, image load by preferences, zoom by preference

--- src/ru/client/habr/RemoveNode.java
+++ src/ru/client/habr/RemoveNode.java
@@ -1,15 +1,21 @@
 package ru.client.habr;
 
-public class RemoveImage 
+public class RemoveNode 
 {
 	/**
 	 * Вызов НЛО для уничтожения картинок в тексте
 	 * @param data текст
 	 * @return текст без картинок
 	 */
-	public static String remove(String data)
+	public static String removeImage(String data)
 	{
 		if(data == null) return null;
 		return data.replaceAll("<img[^>]+src=\"([^\"]+)\"[^>]+>", "<h4 class=\"ufo\">Прилитело НЛО и уничтожило <a href=\"$1\">картинку</a></h4>");
+	}
+	
+	public static String removeVideo(String data)
+	{
+		if(data == null) return null;
+		return data.replaceAll("<object.+<embed src=\"([^\"]+)\".+</object>", "<a href=\"$1\">Здесь был ролик</a>");
 	}
 }
